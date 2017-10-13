cbd9f99f0d6508292dc4a5f531498eb083c98818
RainScorpio <rain.scorpio@hotmail.com>
2017-10-13 19:24:34 +0800

    Node.js基础02_node_modules目录

--- Node_CZ/NodeJS02/test/People.js
+++ Node_CZ/NodeJS02/test/People.js
@@ -10,9 +10,12 @@ People.prototype = {
   }
 };
 
-// 此时, People就被十位构造函数, 可以用new来实例化.
-module.exports = People;
+// exports = People; // 错误, 无法向外界提供People接口, 提供的是{}
 
+// 此时, People就被十位构造函数, 可以用new来实例化.
+module.exports = People; // 推荐方法
+
+// exports.Human = People; // 可行, 但是形式很怪异.
 
 
 
