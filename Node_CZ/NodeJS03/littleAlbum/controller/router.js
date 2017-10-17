02d481986fb88c4107108871cddfbd97e098051c
RainScorpio <rain.scorpio@hotmail.com>
2017-10-17 17:42:01 +0800

    NodeJS03_7. 小小相册--完善封装的获取相册目录的名称函数

--- Node_CZ/NodeJS03/littleAlbum/controller/router.js
+++ Node_CZ/NodeJS03/littleAlbum/controller/router.js
@@ -7,7 +7,13 @@ exports.showIndex = function (req, res) {
 
   // Node.js的编程思维, 就是所有的东西都是异步的
   // 所以, 内层函数不是return数据, 而是调用高层函数提供的回调函数, 把数据当做回调函数的擦那是来使用.
-  file.getAllAlbums(function (allAlabums) {
+  file.getAllAlbums(function (err, allAlabums) {
+
+    if(err) {
+      res.send(err);
+      return;
+    }
+
     res.render('index', {
       'albums': allAlabums
     });
--- Node_CZ/NodeJS03/littleAlbum/modules/file.js
+++ Node_CZ/NodeJS03/littleAlbum/modules/file.js
@@ -9,17 +9,21 @@ exports.getAllAlbums = function (callBack) {
 
   // 模块中的路径参数都是从根目录出发的, 所以是 './uploads'
   fs.readdir('./uploads', function (err, files) {
+    if(err) {
+      callBack('没有找到uploads目录', null);
+    }
+
     var allAlbums = [];
     for (var i = 0; i < files.length; i++) {
       var stat = fs.statSync('./uploads/'+files[i]);
+      // var stat = fs.statSync(files[i]); // 如果传入的路径错误, 会直接就报错, 不会返回错误信息.
       if (stat.isDirectory()) {
         allAlbums.push(files[i]);
       }
     }
-    console.log(allAlbums);
     // return allAlbums; // 出现错误, 由于是异步的.
     // 解决方法 添加回调函数
-    callBack(allAlbums);
+    callBack(null, allAlbums);
     return;
   });
 
